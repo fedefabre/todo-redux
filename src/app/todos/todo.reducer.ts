882d48cdcc65f2ee5566da0636300f842d91146e
Federico Fabre <federicofabre@gmail.com>
2020-07-04 17:20:35 -0300

    first commit

---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,55 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear, toggle, editar, borrar, toggleAll, borrarCompletados } from './todo.actions';
+import { Todo } from './models/todo.model';
+
+export const estadoInicial: Todo[] = [
+    new Todo('Salvar el mundo'),
+    new Todo('Vencer a Thanos'),
+    new Todo('Comprar traje ironman'),
+];
+
+const _todoReducer = createReducer(estadoInicial,
+    on(crear, (state, { texto }) => [...state, new Todo(texto)]),
+    on(borrar, (state, { id }) => state.filter(todo => todo.id !== id)),
+    on(borrarCompletados, state => state.filter(todo => !todo.completado)),
+    on(toggle, (state, { id }) => {
+        return state.map(todo => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    completado: !todo.completado
+                }
+            } else {
+                return todo;
+            }
+
+        });
+    }),
+    on(toggleAll, (state, { completado }) => {
+        return state.map(todo => {
+            return {
+                ...todo,
+                completado: completado
+            }
+
+        });
+    }),
+    on(editar, (state, { id, texto }) => {
+        return state.map(todo => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    texto: texto
+                }
+            } else {
+                return todo;
+            }
+
+        });
+    }),
+);
+
+export function todoReducer(state, action) {
+    return _todoReducer(state, action);
+}
+
